833475e9f8655d23d35cc6fb2d51d012511f4f76
Ohleesang <jammmmed@gmail.com>
2023-12-27 10:13:48 +0900

    간단한 나머지 구하는문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,12 @@
 class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = -1
+        answer = num1%num2
+        return answer
+    }
+}
+fun main(){
+    val a = Solution()
+    a.solution(3,2)//1
+    a.solution(10,5)//0
 }
